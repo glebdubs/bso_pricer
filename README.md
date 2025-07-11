4a0b7710f9499d6895913ad9baffdcab16d4979d
glebdubinin <glebka090107@gmail.com>
2025-07-11 18:11:14 +0930

    progress

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# 🎈 Blank app template
+# Black Scholes Option Pricing Model
 
-A simple Streamlit app template for you to modify!
+Option pricer that gives call and put option prices given by the black scholes model for an option of given specs.
 
-[![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://blank-app-template.streamlit.app/)
+Visible [here](https://gdu-blackscholes.streamlit.app/).
 
-### How to run it on your own machine
+### run it on your own machine
 
-1. Install the requirements
+Install the requirements
 
    ```
    $ pip install -r requirements.txt
    ```
 
-2. Run the app
+Run the app
 
    ```
    $ streamlit run streamlit_app.py
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,55 @@
 import streamlit as st
+import numpy as np
+from scipy.stats import norm
+
+
+
+st.set_page_config(
+  layout="wide",
+  page_title="Black-Scholes Option Pricer",
+  page_icon="🫃"
+)
+
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        width: 300px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        width: 300px;
+        margin-left: -300px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Sidebar content
+st.sidebar.title("Option Properties")
+st.sidebar.divider()
+st.sidebar.markdown("\n")
+
+stockPrice = st.sidebar.number_input("current stock price:", value=100)
+exercisePrice = st.sidebar.number_input("exercise price:", value=110)
+rfir = st.sidebar.number_input("risk-free interest rate:", step=0.01, value = 0.05)
+ttr = st.sidebar.number_input("time to expiration (*years*):", value=2)
+vol = st.sidebar.number_input("volatility (σ): ", value=0.1)
+
+d1 = ( np.log( stockPrice / exercisePrice ) + ttr * (rfir + ( pow(vol, 2) / 2) ) ) / (vol * np.sqrt(ttr))
+
+d2 = ( np.log( stockPrice / exercisePrice ) + ttr * (rfir - ( pow(vol, 2) / 2) ) ) / (vol * np.sqrt(ttr))
+
+callPrice = stockPrice * norm.cdf(d1) - exercisePrice * np.exp( - rfir * ttr) * norm.cdf(d2)
+
+putPrice = np.exp(-rfir * ttr) * exercisePrice * norm.cdf(-d2) - stockPrice * norm.cdf(-d1)
+
 
 st.title("▲ Black-Scholes Option Pricing Model")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.markdown("Using the formulas for _**european**_ options, this tool takes in given properties of an option and returns the appropriate call and put prices.")
+
+st.divider()
+col1, col2 = st.columns([0.4, 0.6], gap="medium")
+
+with col1:
+  st.markdown("Formula for black-scholes pricing of a european **call option**:")
